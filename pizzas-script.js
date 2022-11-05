34756be5b6cf1ee747fc57c73ea594656b875a3a
Bill Sullivan <w.o.sullivan24@gmail.com>
2022-11-05 11:36:56 -0700

    Able to start new pizza and add ingredients. Ingredients list updates for each based on what has not been used

--- pizzas-script.js
+++ pizzas-script.js
@@ -4,9 +4,11 @@ const createBtn = document.querySelector(".create-btn");
 let newPizza;
 
 class NewPizza {
-  toppingsAvailable = new Set(localStorage.getItem("toppings").split(","));
-  toppingsUsed = new Set();
+  allToppings = localStorage.getItem("toppings").split(",");
+  toppingsUsed = [];
+  toppingsAvailable = this.allToppings;
   toppingsCount = 0;
+  addIngBtn;
 
   constructor() {
     this.generateInitialMarkup();
@@ -21,17 +23,20 @@ class NewPizza {
       <li>
         <span>Topping ${this.toppingsCount + 1}: </span>
         <select class="new-pizza-toppings">
-          ${this.generateOptions(this.toppingsAvailable)}
+          <option value="none" selected disabled hidden>Choose one</option>
+          ${this.generateOptions(this.allToppings)}
         </select>
       </li>
+      <button class="add-ing-btn">Add</button>
     </ul>`;
     createBtn.insertAdjacentHTML("afterend", ul);
-    const dropDown = document.querySelector(".new-pizza-toppings");
-    dropDown.addEventListener("change", this.updateToppings.bind(this));
+    this.addDropDownChangeListeners();
+    this.addIngBtn = document.querySelector(".add-ing-btn");
+    this.addIngBtn.addEventListener("click", this.addIngredient.bind(this));
   }
 
   generateOptions(toppings) {
-    let options = `<option value="none" selected disabled hidden>Choose one</option>`;
+    let options = ``;
     toppings.forEach(
       (top) => (options += `<option value="${top}">${top}</option>`)
     );
@@ -39,48 +44,48 @@ class NewPizza {
   }
 
   updateToppings() {
-    console.log("hi");
+    const selectedToppings = [
+      ...document.querySelectorAll(".new-pizza-toppings"),
+    ];
+    const usedToppings = new Set(selectedToppings.map((item) => item.value));
+    this.toppingsAvailable = this.allToppings.filter(
+      (top) => !usedToppings.has(top)
+    );
+    selectedToppings.forEach(
+      (select) =>
+        (select.innerHTML =
+          (select.value === "none"
+            ? `<option value="none" selected disabled hidden>Choose one</option>`
+            : `<option value="${select.value}" selected>${select.value}</option>`) +
+          `${this.generateOptions(this.toppingsAvailable)}`)
+    );
+  }
+
+  addIngredient() {
+    const newIngHTML = `
+      <li>
+        <span>Topping ${this.toppingsCount + 1}: </span>
+        <select class="new-pizza-toppings">
+          <option value="none" selected disabled hidden>Choose one</option>
+          ${this.generateOptions(this.toppingsAvailable)}
+        </select>
+      </li>
+    `;
+    this.addIngBtn.insertAdjacentHTML("beforebegin", newIngHTML);
+    this.addDropDownChangeListeners();
+  }
+
+  addDropDownChangeListeners() {
+    const dropDowns = [...document.querySelectorAll(".new-pizza-toppings")];
+    dropDowns.forEach((drop) =>
+      drop.addEventListener("change", this.updateToppings.bind(this))
+    );
   }
 }
 
 const startNewPizza = function () {
   if (newPizza) return;
   newPizza = new NewPizza();
-  console.log(newPizza);
 };
 
-// const startNewPizza = function () {
-//   const toppingsAvailable = new Set(
-//     localStorage.getItem("toppings").split(",")
-//   );
-//   console.log(toppingsAvailable);
-//   const toppingsUsed = new Set();
-//   const toppingsCount = 0;
-//   const ul = `
-//   <ul class="new-pizza">
-//     <li class="new-pizza-name">
-//       <span>Name: </span><input type="text" />
-//     </li>
-//     <li>
-//       <span>Topping ${toppingsCount + 1}: </span>
-//       <select class="new-pizza-toppings">
-//         ${generateOptions(toppingsAvailable)}
-//       </select>
-//     </li>
-//   </ul>`;
-//   createBtn.insertAdjacentHTML("afterend", ul);
-//   const dropDown = document.querySelector(".new-pizza-toppings");
-//   dropDown.addEventListener("change");
-// };
-
-// const generateOptions = function (toppings) {
-//   let options = ``;
-//   console.log(toppings);
-//   console.log(toppings.size);
-//   toppings.forEach((top) => (options += `<option>${top}</option>`));
-//   console.log(options);
-//   return options;
-// };
-
 createBtn.addEventListener("click", startNewPizza);
-// console.log(localStorage);
